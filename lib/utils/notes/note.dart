dc651fa5ee7dc0f95ddcba554a0a1c2630093b11
Gleb Tihonenko <tihonenkogleeb@gmail.com>
2025-10-26 22:50:13 +0300

    attempt 9

--- lib/utils/notes/note.dart
+++ lib/utils/notes/note.dart
@@ -1,5 +1,5 @@
 import "package:flutter/foundation.dart";
-import "package:hive/hive.dart";
+import "package:hive_flutter/hive_flutter.dart";
 import "package:meta/meta.dart";
 
 import "../log.dart";
